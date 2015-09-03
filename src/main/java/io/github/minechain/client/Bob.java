6e9d39bbf44a323a1aba60b3126ad3e5fadb69a4
TheInfoBug <TheInfoBug@Sean-Laptop>
2015-09-03 17:25:40 -0500

    Push initial client build testing

---
+++ src/main/java/io/github/minechain/client/Bob.java
@@ -0,0 +1,12 @@
+package io.github.minechain.client;
+
+import io.github.minechain.base.TestInstance;
+
+public class Bob {
+
+	public void init() {
+		TestInstance test = new TestInstance();
+
+		test.notify();
+	}
+}
